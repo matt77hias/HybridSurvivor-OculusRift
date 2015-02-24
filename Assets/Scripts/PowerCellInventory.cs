6efaad64f89f1e65dd22ea45d5e826e1cf03b65f
matt77hias <matthias.moulin@gmail.com>
2015-02-24 20:23:30 +0100

    Reorganization

---
+++ Assets/Scripts/PowerCellInventory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerCellInventory : MonoBehaviour {
+	
+	// Cells
+	public static int charge = 0;
+	public AudioClip collectSound;
+	
+	// HUD
+	public Texture2D[] hudCharge;
+	public GUITexture chargeHudGUI;
+
+	// Use this for initialization
+	void Start () {
+		charge = 0;
+		chargeHudGUI.enabled = true;
+	}
+	
+	void CellPickup() {
+		AudioSource.PlayClipAtPoint(collectSound, transform.position);
+		chargeHudGUI.texture = hudCharge[++charge];
+	}
+}
