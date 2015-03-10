7f14e7bc8d86c08e43053377a21600555be6f9f5
matt77hias <matthias.moulin@gmail.com>
2015-03-10 19:14:15 +0100

    Jumpscare added

---
+++ Assets/Scripts/ScareTrigger.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+var face : GameObject;
+var enterTrigger = false;
+var hasPlayed = false;
+var screamingSound : AudioClip;
+
+function Start () {
+	enterTrigger = false;
+	face.GetComponent.<Renderer>().enabled = false;
+}
+
+function Update () {
+	if (enterTrigger == true && hasPlayed == false) {
+		face.GetComponent.<Renderer>().enabled = true;
+		GetComponent.<AudioSource>().PlayOneShot(screamingSound);
+		RemoveOverTime();
+	}
+}
+
+function OnTriggerEnter(other : Collider) {
+	enterTrigger = true;
+}
+
+function RemoveOverTime() {
+	yield WaitForSeconds(1.0);
+	face.GetComponent.<Renderer>().enabled = false;
+	hasPlayed = true;
+}
