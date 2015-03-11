296b55d93c37311689fcff12daa3802730933d09
matt77hias <matthias.moulin@gmail.com>
2015-03-11 21:52:10 +0100

    Slender is now kinematic WIP for jumpscare script

--- Assets/Scripts/ScareTrigger.js
+++ Assets/Scripts/ScareTrigger.js
@@ -4,25 +4,44 @@ var enterTrigger = false;
 var hasPlayed = false;
 var screamingSound : AudioClip;
 
+var startPos: Vector3;
+var endPos: Vector3;
+// Movement speed in units/sec.
+var speed = 0.000001;
+// Time when the movement started.
+private var startTime: float;
+// Total distance between the markers.
+private var journeyLength: float;	
+
 function Start () {
 	enterTrigger = false;
+	startPos = face.transform.position;
+	endPos = transform.position;
 	face.GetComponent.<Renderer>().enabled = false;
 }
 
 function Update () {
 	if (enterTrigger == true && hasPlayed == false) {
-		face.GetComponent.<Renderer>().enabled = true;
 		GetComponent.<AudioSource>().PlayOneShot(screamingSound);
-		RemoveOverTime();
+		
+		var distCovered = (Time.time - startTime) * speed;
+		var fracJourney = distCovered / journeyLength;
+		face.transform.position = Vector3.Lerp(startPos, endPos, fracJourney);
+		
+		if (fracJourney > 1.0) {
+			RemoveOverTime();
+		}
 	}
 }
 
 function OnTriggerEnter(other : Collider) {
 	enterTrigger = true;
+	startTime = Time.time;
+	face.GetComponent.<Renderer>().enabled = true;
 }
 
 function RemoveOverTime() {
-	yield WaitForSeconds(1.0);
+	//yield WaitForSeconds(1.0);
 	face.GetComponent.<Renderer>().enabled = false;
 	hasPlayed = true;
 }
