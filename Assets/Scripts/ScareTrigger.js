310a221dcbf0a2455df53f4958df9966241f3de8
matt77hias <matthias.moulin@gmail.com>
2015-03-12 11:06:36 +0100

    Moving jumpscare added

--- Assets/Scripts/ScareTrigger.js
+++ Assets/Scripts/ScareTrigger.js
@@ -1,22 +1,19 @@
 ﻿#pragma strict
 var face : GameObject;
-var enterTrigger = false;
-var hasPlayed = false;
+private var enterTrigger = false;
+private var hasPlayed = false;
 var screamingSound : AudioClip;
 
-var startPos: Vector3;
-var endPos: Vector3;
-// Movement speed in units/sec.
-var speed = 0.000001;
-// Time when the movement started.
+var player : GameObject;
+private var startPos: Vector3;
+private var endPos: Vector3;
+var speed = 30.0;
 private var startTime: float;
-// Total distance between the markers.
-private var journeyLength: float;	
+private var distance: float;	
 
 function Start () {
 	enterTrigger = false;
 	startPos = face.transform.position;
-	endPos = transform.position;
 	face.GetComponent.<Renderer>().enabled = false;
 }
 
@@ -25,11 +22,12 @@ function Update () {
 		GetComponent.<AudioSource>().PlayOneShot(screamingSound);
 		
 		var distCovered = (Time.time - startTime) * speed;
-		var fracJourney = distCovered / journeyLength;
+		var fracJourney = distCovered / distance;
 		face.transform.position = Vector3.Lerp(startPos, endPos, fracJourney);
 		
 		if (fracJourney > 1.0) {
-			RemoveOverTime();
+			face.GetComponent.<Renderer>().enabled = false;
+			hasPlayed = true;
 		}
 	}
 }
@@ -37,11 +35,7 @@ function Update () {
 function OnTriggerEnter(other : Collider) {
 	enterTrigger = true;
 	startTime = Time.time;
+	endPos = player.transform.position;
+	distance = Vector3.Distance(startPos, endPos);
 	face.GetComponent.<Renderer>().enabled = true;
-}
-
-function RemoveOverTime() {
-	//yield WaitForSeconds(1.0);
-	face.GetComponent.<Renderer>().enabled = false;
-	hasPlayed = true;
 }
